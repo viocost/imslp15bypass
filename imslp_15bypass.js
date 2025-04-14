83f72b2d4fcbd838f02b1afadb7c2a0e98157a5f
Konstantin Y. Rybakov <viocost@gmail.com>
2025-04-14 14:43:57 +0300

    v3 migration

--- imslp_15bypass.js
+++ imslp_15bypass.js
@@ -1,10 +1,12 @@
-	var a = document.getElementById("sm_dl_wait");
-	if (a != undefined){
-			var href = a.dataset["id"];
-			var newDev = document.createElement("div");
-			newDev.innerHTML = "<a href='" + href + "'>DOWNLOAD NOW</a>";
-
-			document.getElementsByClassName('body')[0].getElementsByTagName("center")[0].replaceChild(newDev, a);
-			window.open(href, "_blank");
-			window.history.back()
-	}
+var a = document.getElementById("sm_dl_wait");
+if (a != undefined) {
+  var href = a.dataset["id"];
+  var newDev = document.createElement("div");
+  newDev.innerHTML = "<a href='" + href + "'>DOWNLOAD NOW</a>";
+  document
+    .getElementsByClassName("body")[0]
+    .getElementsByTagName("center")[0]
+    .replaceChild(newDev, a);
+  window.open(href, "_blank");
+  window.history.back();
+}
